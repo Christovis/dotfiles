4fd98b3815f88bb6c28d72b795f97f77ef6cb07d
christovis <chrbecker01@gmail.com>
2020-07-07 00:02:46 +0100

    update dotfiles installation infrastructure

--- README.md
+++ README.md
@@ -2,6 +2,5 @@
    <img src="https://github.com/Christovis/dotfiles/blob/master/dotfiles-logo-stacked.png" alt="dotfiles stacked logo" width="100">
 </p>
 
-# Dotfiles
 This repo contains my dotfiles. It provides default configuration for the set of
 applications that I use every day in the terminal
--- install.sh
+++ install.sh
@@ -1,35 +1,291 @@
 #!/bin/bash
 ############################
-# .make.sh
 # This script creates symlinks from the home directory to
 # any desired dotfiles in ~/dotfiles
+#
+# Requirements:
+#    - wmctrl
+#
+# Inspired by https://github.com/iamrecursion/dotfiles
 ############################
 
-########## Variables
-dir=~/dotfiles                    # dotfiles directory
-olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bashrc i3blocks.conf vimrc zshrc xinitrc Xmodmap Xresources zshrc compton.conf"    # list of files/folders to symlink in homedir
-files_config="i3 termite"
-##########
+# Current linux window manager (e.g. Gnome, i3)
+wm=""
 
-# create dotfiles_old in homedir
-echo "Creating $olddir for backup of any existing dotfiles in ~"
-mkdir -p $olddir
-echo "...done"
+# Functions
+wm_gnome="gnome"
+wm_i3="i3"
 
-# change to the dotfiles directory
-echo "Changing to the $dir directory"
-cd $dir
-echo "...done"
+# Backup Existing Files
+backup_dir="${HOME}/dotfilesbackup"
+update_dir="${HOME}/.dotfiles"
+system_deps=""
 
-# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
-for file in $files; do
-	    echo "Moving any existing dotfiles from ~ to $olddir"
-	        mv ~/.$file ~/dotfiles_old/
-		    echo "Creating symlink to $file in home directory."
-		        ln -s $dir/$file ~/.$file
-		done
-for file_config in $files_config; do
-  mkdir -p ~/.config/$file_config
-  ln -s $dir/$file_config ~/.config/$file_config/config
-done
+# Script Configuration
+status_prefix="INFO >>";
+
+
+# -----------------------------------------------------------------------------
+detect_wm () {
+    # identify the window manager that is used
+    if [[ $(wmctrl -m) =~ 'GNOME' ]]; then
+        wm=${wm_gnome}
+    elif [[ $(wmctrl -m) =~ 'i3' ]]; then
+        wm=${wm_i3}
+    else
+        >&2 echo "Unknown window manager";
+        return 1;
+    fi
+}
+
+
+confirm() {
+    local prompt default reply pdefault
+    prompt="${1}"
+
+    if [ "${2:-}" = "Y" ]; then
+        pdefault="Y/n"
+        default=Y
+    elif [ "${2:-}" = "N" ]; then
+        pdefault="y/N"
+        default=N
+    else
+        pdefault="y/n"
+        default=
+    fi
+
+    while true; do
+
+        # Ask the question (not using "read -p" as it uses stderr not stdout)
+        echo -n "${prompt} [${pdefault}] "
+
+        # Read the answer (use /dev/tty in case stdin is redirected from
+        # somewhere else)
+        read -r reply </dev/tty
+
+        # Default?
+        [ -z "${reply// }" ] && reply=${default}
+
+        # Check if the reply is valid
+        case "${reply}" in
+            Y*|y*) return 0 ;;
+            N*|n*) return 1 ;;
+        esac
+
+    done
+}
+
+
+confirm_string() {
+    local prompt default reply pdefault
+    prompt="${1}"
+    default="${2}"
+
+    # Default?
+    [[ -n "${default// }" ]] && pdefault=" [${default}]" || pdefault=""
+
+    # Ask the question
+    read -r -p "${prompt}${pdefault} " reply
+
+    # Empty reply?
+    echo "${reply:-$default}"
+}
+
+
+install_system_package() {
+    [[ -z "${*// }" ]] && return 0
+
+error= << ERR
+"Installing system packages currently not compatible with your package manager.
+ERR
+
+    if [[ "${wm}" == "${wm_gnome}" ]] ; then
+        if [ -n "$(command -v yay)" ]; then
+            yay -Syu && yay -S ${@}
+        elif [ -n "$(command -v pacman)" ]; then
+            sudo pacman -Syu && sudo pacman -S ${@}
+        elif [ -n "$(command -v apt-get)" ]; then
+            sudo apt-get update -qq && sudo apt-get install ${@} -y
+        else
+            >&2 echo $error
+            return 1
+        fi
+    else
+        >&2 echo "Unknown window manager"
+        return 1
+    fi
+}
+
+
+preinstall () {
+cat << STATUS
+============================ SETTING UP ENVIRONMENT ===========================
+STATUS
+
+    # Cloning Submodules
+    echo "${status_prefix} Cloning Submodules"
+
+    cd ${DIR};
+    git submodule update --init --recursive;
+    cd ${HOME};
+}
+
+
+link_zsh () {
+    echo "${status_prefix} Linking ZSH Configuration"
+
+    ln -sf ${DIR}/zsh/zshrc ${HOME}/.zshrc
+
+    return 0
+}
+
+
+link_neovim () {
+    echo "${status_prefix} Linking Neovim Configuration"
+
+    nvimrc_dir="${HOME}/.config/nvim"
+    nvim_plugs_dir="${HOME}/.local/share/nvim/site"
+
+    if [ ! -d  "${nvimrc_dir}" ]; then
+        echo "${status_prefix} Creating "${nvimrc_dir}" for Nvim \
+            configuration";
+        mkdir -p "${nvimrc_dir}"
+    fi
+
+    ln -sf "${DIR}/nvim/init.vim" "${nvimrc_dir}/init.vim"
+    ln -sf "${DIR}/nvim/coc-settings.json" "${nvimrc_dir}/coc-settings.json"
+
+    if [ ! -d "${nvim_plugs_dir}" ]; then
+        echo "${status_prefix} Creating "${nvim_plugs_dir}" for plugins";
+        mkdir -p "${nvim_plugs_dir}"
+    fi
+
+    if [ ! -e "${nvim_plugs_dir}/autoload" ]; then
+        ln -sf "${DIR}/nvim/autoload" "${nvim_plugs_dir}/autoload"
+    fi
+
+    return 0
+}
+
+
+link_tmux () {
+    echo "${status_prefix} Linking Tmux configuration"
+
+    ln -sf "${DIR}/tmux/tmux.conf" "${HOME}/.tmux.conf"
+
+    return 0
+}
+
+
+link_gdb () {
+    echo "${status_prefix} Linking GDB Configuration"
+
+    ln -sf "${DIR}/tools/gdbinit" "${HOME}/.gdbinit"
+
+    return 0
+}
+
+
+create_gitconfig () {
+    target_file="${HOME}/.gitconfig"
+
+    confirm "Create gitconfig?" || return 0
+
+    echo "${status_prefix} Creating Git Configuration"
+
+    cp "${DIR}/git/gitconfig" "${target_file}"
+
+    if [[ ${wm} == "${wm_gnome}" ]]; then
+        response=$(confirm_string "Enter git user.name" "$(git config --get user.name)")
+        sed -i "s/##git_name##/${response}/g" ${target_file}
+
+        response=$(confirm_string "Enter git user.email" "$(git config --get user.email)")
+        sed -i "s/##git_email##/${response}/g" ${target_file}
+
+        response=$(confirm_string "Enter git user.signingkey" "$(git config --get user.email)")
+        sed -i "s/##git_signingkey##/${response}/g" ${target_file}
+    else
+        echo "Unknown window manager ${wm}. Cannot execute sed."
+    fi
+
+    # Global Gitignore
+    ignore_source_file="${DIR}/git/gitignore_global"
+    ignore_target_file="${HOME}/.gitignore_global"
+
+    ln -sf ${ignore_source_file} ${ignore_target_file}
+
+    return 0
+}
+
+
+install () {
+    link_zsh || exit 1
+    link_neovim || exit 1
+    link_tmux || exit 1
+    link_gdb || exit 1
+
+    create_gitconfig || exit 1
+}
+
+
+postinstall () {
+    echo "${status_prefix} Rewriting Master URL to Use SSH"
+
+    cd ${DIR} > /dev/null || return;
+    git remote set-url origin git@github.com:Christovis/dotfiles.git;
+    cd ${HOME} > /dev/null || return;
+
+cat << EOM
+================================= PLEASE READ =================================
+If using TMUX, please start it and execute 'C-a' + 'I' to install plugins.
+If using zsh, please source '~/.zshrc' and execute 'zplug install'.
+If using nvim or vim, please open the respective editor and run ':PlugInstall'
+to install the plugin configuration. This step will likely require system
+dependencies for the various plugins. These include:
+  - rustc
+  - ghc
+  - stack
+For IDE functionality in neovim, you will likely need to install the various
+language servers. These are listed in \`nvim/init.vim\` in the section entitled
+'Coc.nvim Configuration'. Nvim will still work without these installed, but none
+of the IDE functionality will be available.
+EOM
+}
+
+
+# -----------------------------------------------------------------------------
+# Main
+
+main () {
+    # Getting Script Directory
+    SOURCE="${BASH_SOURCE[0]}"
+
+    while [ -h "${SOURCE}" ]; do # resolve $SOURCE until file no longer a link
+      DIR="$( cd -P "$( dirname "${SOURCE}" )" >/dev/null 2>&1 && pwd )"
+      SOURCE="$(readlink "${SOURCE}")"
+      [[ ${SOURCE} != /* ]] && SOURCE="${DIR}/${SOURCE}" # resolve rel symlinks
+    done
+
+    DIR="$( cd -P "$( dirname "${SOURCE}" )" >/dev/null 2>&1 && pwd )"
+
+    # Install Setup
+    detect_wm || exit 1
+    confirm "Install dotfiles for ${wm} window manager?"|| exit
+    install_system_package "${system_deps}" || exit 1
+    
+    # Installs
+    preinstall
+    install
+    postinstall  # Copy and Edit Gitconfig
+}
+
+# -----------------------------------------------------------------------------
+# Run
+
+if (( ${BASH_VERSION%%.*} < 4 )) ; then
+    >&2 echo "Requires bash version 4.0.0 or higher, you've got ${BASH_VERSION} in /bin/bash"
+    >&2 echo "You may have a newer version elsewhere. Use it to run this script directly."
+    exit 1
+fi
+
+main
